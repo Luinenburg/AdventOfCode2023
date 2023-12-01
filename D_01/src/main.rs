d8f952818b4beec078ccad53c33bce3fcc749f29
CadeLui <videogamefpslover@gmail.com>
2023-12-01 00:59:42 -0500

    day 1 part 1

--- D_01/src/main.rs
+++ D_01/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main()
+{
+    let binding = fs::read_to_string("input.txt")
+        .expect("Should have been able to read file");
+    let mut file_contents: Vec<_> = binding.split("\n").collect();
+
+    let mut nums: Vec<i32> = Vec::new();
+    
+    while let Some(y) = file_contents.pop()
+    {
+        let mut num: String = String::new();
+        for c in y.chars()
+        { if c.is_numeric() { num.push(c); break; } }
+        for c in y.chars().rev()
+        { if c.is_numeric() { num.push(c); break; } }
+        match num.parse() {
+            Ok(n) => nums.push(n),
+            Err(e) => println!("oh shit"),
+        }
+    }
+    let mut i = 0;
+    while let Some(y) = nums.pop()
+    {
+        i += y;
+    }
+    println!("{i}");
 }
---
+++ D_01/target/debug/D_01.d
@@ -0,0 +1,1 @@
+/home/cade/Projects/AdventOfCode2023/D_01/target/debug/D_01: /home/cade/Projects/AdventOfCode2023/D_01/src/main.rs
