3880840d38c49cfd17be0bfd3d081971f0a3f4b8
CadeLui <videogamefpslover@gmail.com>
2023-12-04 12:04:01 -0500

    linter button

--- d_04/src/main.rs
+++ d_04/src/main.rs
@@ -3,38 +3,22 @@ fn main() {
         .trim_end()
         .lines()
         .map(|line| {
-            let split_line = line.split_once(": ")
-                .unwrap().1
-                .split_once(" | ")
-                .unwrap();
+            let split_line = line.split_once(": ").unwrap().1.split_once(" | ").unwrap();
             (
-                split_line.0
-                    .split(' ')
-                    .flat_map(str::parse)
-                    .collect(),
-                split_line.1
-                    .split(' ')
-                    .flat_map(str::parse)
-                    .collect()
+                split_line.0.split(' ').flat_map(str::parse).collect(),
+                split_line.1.split(' ').flat_map(str::parse).collect(),
             )
         })
         .collect();
     let powers: Vec<usize> = data_input
         .iter()
-        .map(
-            |data|
-                data.1
-                    .iter()
-                    .filter(
-                        |x|
-                            data.0
-                                .contains(x))
-                    .count())
+        .map(|data| data.1.iter().filter(|x| data.0.contains(x)).count())
         .collect();
     let mut sum = 0;
-    for i in powers
-    {
-        if i > 0 { sum += 1 << (i-1) };
+    for i in powers {
+        if i > 0 {
+            sum += 1 << (i - 1)
+        };
     }
 
     println!("{}", sum);
