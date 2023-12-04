c596a198d0714eaa6d0d1211deee09611d5e5365
CadeLui <videogamefpslover@gmail.com>
2023-12-04 12:16:33 -0500

    slightly better readability, also part 2 start

--- d_04/src/main.rs
+++ d_04/src/main.rs
@@ -1,3 +1,26 @@
+fn wins_per_game(data_input: &[(Vec<i32>, Vec<i32>)]) -> Vec<usize>
+{
+    data_input
+        .iter()
+        .map(|data| data.1.iter().filter(|x| data.0.contains(x)).count())
+        .collect()
+}
+
+fn part_two(data_input: &[(Vec<i32>, Vec<i32>)]) -> i32 {
+    unimplemented!()
+}
+
+fn part_one(data_input: &[(Vec<i32>, Vec<i32>)]) -> i32 {
+    let powers = wins_per_game(data_input);
+    let mut sum = 0;
+    for i in powers {
+        if i > 0 {
+            sum += 1 << (i - 1)
+        };
+    }
+    sum
+}
+
 fn main() {
     let data_input: Vec<(Vec<i32>, Vec<i32>)> = include_str!("input.txt")
         .trim_end()
@@ -10,16 +33,9 @@ fn main() {
             )
         })
         .collect();
-    let powers: Vec<usize> = data_input
-        .iter()
-        .map(|data| data.1.iter().filter(|x| data.0.contains(x)).count())
-        .collect();
-    let mut sum = 0;
-    for i in powers {
-        if i > 0 {
-            sum += 1 << (i - 1)
-        };
-    }
 
-    println!("{}", sum);
+
+
+    println!("{}", part_one(&data_input));
+    println!("{}", part_two(&data_input));
 }
