8790fba6760c21361d86196638c4c020129c3913
Cade Luinenburg <clui83@outlook.com>
2023-12-08 10:53:31 -0500

    day 07 progress

---
+++ d_07/src/main.rs
@@ -0,0 +1,116 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
+#[derive(PartialEq, Debug)]
+enum Hands {
+    FiveOfAKind(usize),
+    FourOfAKind(usize),
+    FullHouse(usize),
+    ThreeOfAKind(usize),
+    TwoPair(usize),
+    OnePair(usize),
+    HighCard(usize),
+    None,
+}
+
+fn convert_hand_to_value(hand: &str) -> (Hands, Vec<usize>) {
+    let card_values: HashMap<char, usize> = HashMap::from([
+        ('2', 0),
+        ('3', 1),
+        ('4', 2),
+        ('5', 3),
+        ('6', 4),
+        ('7', 5),
+        ('8', 6),
+        ('9', 7),
+        ('T', 8),
+        ('J', 9),
+        ('Q', 10),
+        ('K', 11),
+        ('A', 12),
+    ]);
+    let mut card_count: [usize; 13] = [0; 13];
+
+    let hand_with_values: Vec<usize> = hand
+        .chars()
+        .map(|card| *card_values.get_key_value(&card).unwrap().1)
+        .collect();
+    for value in &hand_with_values {
+        card_count[*value] += 1;
+    }
+
+    let collected_hands: Vec<_> = card_count
+        .map(|card| {
+            let mut hand_val: Hands = Hands::None;
+            if card == 5 {
+                hand_val = Hands::FiveOfAKind(6);
+            }
+            if card == 4 {
+                hand_val = Hands::FourOfAKind(5);
+            }
+            if card == 3 {
+                hand_val = Hands::ThreeOfAKind(3);
+            }
+            if card == 2 {
+                hand_val = Hands::OnePair(1);
+            }
+            if card == 1 {
+                hand_val = Hands::HighCard(0);
+            }
+            hand_val
+        })
+        .into_iter()
+        .filter(|hand_type| *hand_type != Hands::None)
+        .collect();
+    let mut best_hand = {
+        let mut bh = Hands::None;
+        if collected_hands.contains(&Hands::HighCard(0)) {
+            bh = Hands::HighCard(0);
+        }
+        if collected_hands.contains(&Hands::OnePair(1)) {
+            bh = Hands::OnePair(1);
+        }
+        if collected_hands.contains(&Hands::ThreeOfAKind(3)) {
+            bh = Hands::ThreeOfAKind(3);
+        }
+        if collected_hands.contains(&Hands::FourOfAKind(5)) {
+            bh = Hands::FourOfAKind(5);
+        }
+        if collected_hands.contains(&Hands::FiveOfAKind(6)) {
+            bh = Hands::FiveOfAKind(6);
+        }
+        bh
+    };
+    if collected_hands
+        .iter()
+        .filter(|hand| **hand == Hands::OnePair(1))
+        .count()
+        == 2
+    {
+        best_hand = Hands::TwoPair(2);
+    }
+    if collected_hands.contains(&Hands::OnePair(1))
+        && collected_hands.contains(&Hands::ThreeOfAKind(3))
+    {
+        best_hand = Hands::FullHouse(4);
+    }
+    (best_hand, hand_with_values)
+}
+
+fn main() {
+    let data_input: Vec<(&str, usize)> = include_str!("example.txt")
+        .trim_end()
+        .lines()
+        .map(|line| {
+            let (hand, bet) = line.split_whitespace().collect_tuple().unwrap();
+            (hand, bet.parse::<usize>().unwrap())
+        })
+        .collect();
+
+    //println!("{:#?}", data_input);
+    println!("{:#?}", convert_hand_to_value(data_input[2].0));
+    let converted_data: Vec<((Hands, Vec<usize>), usize)> = data_input
+        .iter()
+        .map(|hand| (convert_hand_to_value(hand.0), hand.1))
+        .collect();
+}
